cfbd900d4690f213b161e9c33e14da8645cd2574
Steve McClellan <steve470@gmail.com>
2017-02-16 12:29:07 -0500

    Reformat code as per submission checklist and PEP8

--- media.py
+++ media.py
@@ -1,7 +1,15 @@
 """Container for classes of media items."""
 
+
 class Movie(object):
-    """Stores some basic data about a film."""
+    """Stores some basic data about a film.
+
+    Attributes:
+        title (str): The movie's title.
+        year (int): The year of release.
+        poster_image_url (str): A URL for an image of the movie's poster.
+        trailer_youtube_url (str): Location of the trailer on Youtube.
+    """
 
     def __init__(self, title, year, poster_image_url, trailer_youtube_url):
         self.title = title
